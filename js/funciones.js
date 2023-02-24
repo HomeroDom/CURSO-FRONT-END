606e2a5f6f3caab8fa73276600a6993ce2979c41
= <=>
2023-02-24 19:04:31 -0300

    Primer escritura en repositorio

---
+++ js/funciones.js
@@ -0,0 +1,7 @@
+document.getElementById('boton_cambiarcolor').addEventListener('click', function() {
+    document.body.style.backgroundColor =  '#2CDDE8';
+});
+
+document.getElementById('boton_pordefecto').addEventListener('click', function() {
+    document.body.style.backgroundColor =  '#8b8f91';
+});
